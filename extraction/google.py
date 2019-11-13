80abeb96f68cf7b136482780ffd92e6cfc34f6df
m-i-n-a-r <glucaconti.952@gmail.com>
2019-11-13 08:52:07 +0100

    Basic data extraction and save to csv

--- extraction/google.py
+++ extraction/google.py
@@ -1,1 +1,45 @@
 # Google data extraction - use Google Trends to save the daily searches of one or more keywords
+
+from pytrends.request import TrendReq
+
+# Variabled needed (tries to take it from user or use a default array)
+separator = ','
+data_path = 'data/google/'
+input_keywords = input('Insert an array of comma separated keywords: ')
+if(not input_keywords.strip()): keywords = ['trump', 'clinton']
+else: keywords = input_keywords.split(separator)
+file_name = data_path + '_'.join(keywords).replace(' ', '') + '_interest_over_time.csv'
+
+
+# Login to Google. Only need to run this once, the rest of requests will use the same session.
+pytrend = TrendReq()
+# Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() & related_queries()
+pytrend.build_payload(kw_list=keywords)
+
+# Interest Over Time
+interest_over_time_df = pytrend.interest_over_time()
+print(interest_over_time_df)
+print('')
+interest_over_time_df.to_csv(file_name, sep=';', encoding='utf-8')
+
+# Related Queries, returns a dictionary of dataframes
+related_queries_dict = pytrend.related_queries()
+print(related_queries_dict)
+print('')
+
+# Get Google Keyword Suggestions
+suggestions_dict = pytrend.suggestions(keyword=keywords[0])
+print(suggestions_dict)
+print('')
+
+# Get Google Hot Trends data
+#trending_searches_df = pytrend.trending_searches()
+#print(trending_searches_df.head())
+
+# Get Google Today Hot Trends data
+#today_searches_df = pytrend.today_searches()
+#print(today_searches_df.head())
+
+# Get Google Top Charts
+#top_charts_df = pytrend.top_charts(2018, hl='en-US', tz=300, geo='GLOBAL')
+#print(top_charts_df.head())
--- extraction/twitter.py
+++ extraction/twitter.py
@@ -1,1 +1,31 @@
 # Twitter data extraction - use Tweepy to extract the data about a group of hashtags
+
+import tweepy
+import csv
+from twitter_app_credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Variabled needed (tries to take it from user or use a default array)
+separator = ','
+data_path = 'data/twitter/'
+input_keyword = input('Insert a keyword: ')
+if(not input_keyword.strip()): keyword = 'trump'
+else: keyword = input_keyword
+file_name = data_path + keyword.replace(' ', '') + '_twitter_interest.csv'
+
+# Open/create a file to append data to
+csv_file = open(file_name, 'a')
+
+#Use csv writer
+csv_writer = csv.writer(csv_file)
+tweets = tweepy.Cursor(api.search, q=keyword, since="2014-02-14", until="2014-02-15", lang="en")
+print(tweets)
+
+for tweet in tweets.items():
+    # Write a row to the CSV file. I use encode UTF-8
+    csv_writer.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    print(tweet.created_at, tweet.text)
+csv_file.close()
