eb944a41364a4f1fb158a1144a0c4d57fe3c782a
m-i-n-a-r <glucaconti.952@gmail.com>
2020-01-28 12:42:49 +0100

    Semantic matching using Wordnet, better text processing

--- keyword_matcher.py
+++ keyword_matcher.py
@@ -2,16 +2,23 @@
 
 import difflib
 import re
+from sematch.semantic.similarity import WordNetSimilarity
 
 # Process the trends to be easily comparable, keeping the original strings
-def text_processing(trend_list):
+def text_processing(trend_list, keep_spaces = False):
     processed_list = []
     if(not isinstance(trend_list, list)): return []
     for trend in trend_list:
         processed_trend = {}
         processed_trend['original'] = trend
-        re.sub(' +', ' ', trend) # Remove multiple spaces (redundant, but useful to keep the single spaces if needed)
-        processed_trend['processed'] = trend.lower().replace('.', '').replace('-', '').replace('_', '').replace(' ', '')
+        trend = re.sub(' +', ' ', trend) # Remove multiple spaces (redundant, but useful to keep the single spaces if needed)
+        # Keep the spaces in the smartest way possible
+        if(keep_spaces == True): 
+            a = re.compile('((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))')
+            trend = a.sub(r' \1', trend).lower()
+            trend = re.sub(' +', ' ', trend)
+            processed_trend['processed'] = trend.replace('.', ' ').replace('-', ' ').replace('_', ' ')
+        else: processed_trend['processed'] = trend.lower().replace('.', '').replace('-', '').replace('_', '').replace(' ', '')
         processed_list.append(processed_trend)
     return processed_list
 
@@ -33,11 +40,17 @@ def difflib_matching(trend_one, trend_two):
     if(len(matches) == 0): return 'No matches'
     return matches
 
-# Compare two lists of trends using a semantic approach TODO 
+# Compare two lists of trends using a semantic approach
 def semantic_matching(trend_one, trend_two):  
-    trend_one_processed = text_processing(trend_one)
-    trend_two_processed = text_processing(trend_two) 
-    return
+    treshold = 0.2
+    trend_one_processed = text_processing(trend_one, keep_spaces = True)
+    trend_two_processed = text_processing(trend_two, keep_spaces = True)
+    # The options are Wordnet, YAGO and DBpedia
+    wns = WordNetSimilarity()
+    matches = list({x['original'] for x in trend_one_processed for y in trend_two_processed 
+                    if wns.word_similarity(x['processed'], y['processed'], 'li') > treshold})
+    
+    return matches
 
 # Build a dictionary with the matches between each combination of the three sources using the above functions
 def get_all_matches(google_trending, twitter_trending, wikipedia_trending):
@@ -49,3 +62,10 @@ def get_all_matches(google_trending, twitter_trending, wikipedia_trending):
     matches['twitter-wikipedia'] = lexical_matching(twitter_trending, wikipedia_trending)
     matches['google-twitter-wikipedia'] = lexical_matching(main_match, wikipedia_trending)
     return matches
+
+# Avoid to run the script when imported
+if __name__ == '__main__':
+    # Quick test
+    list_one = ['panda', 'DonaldTrump', 'NCIS', 'coronavirus', 'Kobe  Bryant', 'dog']
+    list_two = ['PandaBear', 'trumpOut', 'ncis', 'virus', 'Kobe', 'test', 'cat']
+    print(semantic_matching(list_one, list_two))
---
+++ searsen_analyzer.py
@@ -0,0 +1,13 @@
+# Provide some useful functions to analyze the dataset
+
+# Given a keyword, check how many hours it was in the trends
+def trend_lifecycle(trend):
+    return
+
+# Compare two set of trends shifting them in time (eg Twitter trends at 7 AM vs Google trends at 9 AM)
+def trend_shifted_comparison():
+    return
+
+# Estimate the polarization for a selected keyword
+def estimate_polarization():
+    return
