ca41dc3fe7a9ad551bf690f8fb5e06126b14230f
m-i-n-a-r <glucaconti.952@gmail.com>
2020-01-29 17:30:43 +0100

    Catalog trends

--- searsen_analyzer.py
+++ searsen_analyzer.py
@@ -14,5 +14,58 @@ def trend_shifted_comparison():
 def estimate_polarization():
     return
 
-# Main part of the script, use the above functions to compute results
+# Iterate over all the documents and count
+def catalog_trends(trends, source):
+    match_counter = {}
+    for document in trends:
+        matches = []   
+        if(source == 'google'): matches = document['google']
+        elif(source == 'twitter'): matches = document['twitter']
+        elif(source == 'wikipedia'): matches = document['wikipedia']
+        elif(source == 'twitter-google'): matches = document['matches']['twitter-google']
+        elif(source == 'twitter-wikipedia'): matches = document['matches']['twitter-wikipedia']
+        elif(source == 'google-wikipedia'): matches = document['matches']['google-wikipedia']
+        elif(source == 'google-twitter-wikipedia'): matches = document['matches']['google-twitter-wikipedia']
+        else: matches = document['google']
+
+        if(isinstance(matches, list) and matches):
+            for trend in matches:
+                if trend in match_counter: match_counter[trend] += 1
+                else: match_counter[trend] = 1
+
+    # Sort by value
+    match_counter = {k: v for k, v in sorted(match_counter.items(), key=lambda item: item[1])}
+    return match_counter
+
+
+# Main part of the searsen analyzer, use the above functions to compute results
 client = MongoClient('mongodb://127.0.0.1:27017')
+db = client.searsendb_us
+
+result = db.trends.find()
+google = catalog_trends(result, 'google')
+result = db.trends.find()
+twitter = catalog_trends(result, 'twitter')
+result = db.trends.find()
+wikipedia = catalog_trends(result, 'wikipedia')
+result = db.trends.find()
+twitter_google = catalog_trends(result, 'twitter-google')
+result = db.trends.find()
+twitter_wikipedia = catalog_trends(result, 'twitter-wikipedia')
+result = db.trends.find()
+google_wikipedia = catalog_trends(result, 'google-wikipedia')
+result = db.trends.find()
+google_twitter_wikipedia = catalog_trends(result, 'google-twitter-wikipedia')
+
+print('''
+*************** SEARSEN ***************
+Automatic trend and sentiment dataset analyzer
+''')
+
+print('\nGOOGLE:\n' + str(google))
+print('\nTWITTER:\n' + str(twitter))
+print('\nWIKIPEDIA:\n' + str(wikipedia))
+print('\nTWITTER-GOOGLE:\n' + str(twitter_google))
+print('\nTWITTER-WIKIPEDIA:\n' + str(twitter_wikipedia))
+print('\nGOOGLE-WIKIPEDIA:\n' + str(google_wikipedia))
+print('\nGOOGLE-TWITTER-WIKIPEDIA:\n' + str(google_twitter_wikipedia) + '\n')
